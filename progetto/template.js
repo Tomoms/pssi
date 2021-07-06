97be328f77f4159eb3959d43932930f8e6164ec8
Tommaso Fonda <fondatommaso2@gmail.com>
2021-07-06 14:48:12 +0200

    Template JS updates

--- progetto/template.js
+++ progetto/template.js
@@ -1,12 +1,16 @@
 const base = "https://script.google.com/macros/s/AKfycbxRfgB25xhkN6xLvTuwRA7bvPcTYCLdsoDI9VlH4REXXLv3kdMd6BIjm6mg9EDUppGd/exec?GSheet=https://docs.google.com/spreadsheets/d/1OUjljb3Xa-9ZKMXsmWBFYooffbRPLKAIwIGLXDDPLAE/edit"
 
+var modal;
+var phonyDiv;
+var contentDiv;
+
 function updateEditButtons() {
-   const elements = document.getElementsByClassName('awt-button edit');
+    const elements = document.getElementsByClassName('awt-button edit');
     for (const element of elements) {
         element.onclick = function(event) {
             event.preventDefault();
-            let airtableID = this.href.split(":")[1];
-            editElement(airtableID);
+            editElement(this.href.split(":")[1]);
+            return false;
         }
     }
 }
@@ -15,7 +19,6 @@ async function getUpdatableCols(key) {
     let url = base + "&type=getFields&key=" + key;
     let response = await fetch(url, {
         method: 'GET',
-        //credentials: 'include',
     });
     let result = await response.text();
     let asString = String(result);
@@ -53,8 +56,8 @@ async function editElement(key) {
 }
 
 async function confirm() {
-  let modalCnf = document.querySelector('.confirmation');
-  modalCnf.style.display = 'block';
+    let modalCnf = document.querySelector('.confirmation');
+    modalCnf.style.display = 'block';
 }
 
 async function saveElement() {
@@ -68,7 +71,6 @@ async function saveElement() {
     }
     let response = await fetch(updated, {
         method: 'GET',
-        //credentials: 'include'
     });
     let result = await response.text();
     if (String(result) != "") {
@@ -76,7 +78,7 @@ async function saveElement() {
     }
     modal.style.display = 'none';
     phonyDiv.textContent = '';
-    setTimeout(refreshPage(), 2000);
+    setTimeout(refreshPage(), 1500);
 }
 
 async function deleteElement() {
@@ -85,7 +87,6 @@ async function deleteElement() {
     let url = base + "&type=deleteElement&key=" + key
     let response = await fetch(url, {
         method: 'GET',
-        //credentials: 'include'
     });
     let result = await response.text();
     if (String(result) != "") {
@@ -93,7 +94,7 @@ async function deleteElement() {
     }
     modal.style.display = 'none';
     phonyDiv.textContent = '';
-    setTimeout(refreshPage(), 2000);
+    setTimeout(refreshPage(), 1500);
 }
 
 
@@ -101,95 +102,94 @@ async function refreshPage() {
     window.parent.location = document.referrer;
 }
 
+async function initEverything() {
+    var element = document.getElementById("dashboard");
+    var modalDiv = document.createElement("div");
+    modalDiv.className = "modal";
+    contentDiv = document.createElement("div");
+    contentDiv.className = "modal-content";
+    var title = document.createElement("h4");
+    title.id = 'modalTitle';
+    var text = document.createTextNode("Modal window");
+    title.appendChild(text);
+    var hr = document.createElement("hr");
+    phonyDiv = document.createElement("div");
 
-var element = document.getElementById("dashboard");
-var modalDiv = document.createElement("div");
-modalDiv.className = "modal";
-var contentDiv = document.createElement("div");
-contentDiv.className = "modal-content";
-var title = document.createElement("h4");
-title.id = 'modalTitle';
-//placeholder title
-var text = document.createTextNode("Modal window");
-title.appendChild(text);
-var hr = document.createElement("hr");
-var phonyDiv = document.createElement("div");
+    var closeBtn = document.createElement("button");
+    var closeImg = document.createElement("img");
+    closeImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/close.png';
+    closeImg.style.height = '30px';
+    closeImg.style.width = '30px';
+    closeBtn.style.marginRight = '20px';
+    closeBtn.appendChild(closeImg);
+    closeBtn.onclick = function() {
+        modal.style.display = 'none';
+        phonyDiv.textContent = '';
+    };
 
+    var saveBtn = document.createElement("button");
+    var saveImg = document.createElement("img");
+    saveImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/save.png';
+    saveImg.style.height = '30px';
+    saveImg.style.width = '30px';
+    saveBtn.style.marginRight = '20px';
+    saveBtn.appendChild(saveImg);
+    saveBtn.onclick = saveElement;
 
-var closeBtn = document.createElement("button");
-closeBtn.className = "close-img";
-var closeImg = document.createElement("img");
-closeImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/close.png';
-closeImg.style.height = '30px';
-closeImg.style.width = '30px';
-closeBtn.appendChild(closeImg);
-closeBtn.onclick = function() {
-    modal.style.display = 'none';
-    phonyDiv.textContent = '';
-};
+    var deleteBtn = document.createElement("button");
+    deleteBtn.className = "save-img";
+    var deleteImg = document.createElement("img");
+    deleteImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/delete.png';
+    deleteImg.style.height = '30px';
+    deleteImg.style.width = '30px';
+    deleteBtn.appendChild(deleteImg);
+    deleteBtn.onclick = confirm;
 
-var saveBtn = document.createElement("button");
-saveBtn.className = "save-img";
-var saveImg = document.createElement("img");
-saveImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/save.png';
-saveImg.style.height = '30px';
-saveImg.style.width = '30px';
-saveBtn.appendChild(saveImg);
-saveBtn.onclick = saveElement;
+    contentDiv.appendChild(title);
+    contentDiv.appendChild(hr);
+    contentDiv.appendChild(phonyDiv);
+    contentDiv.appendChild(closeBtn);
+    contentDiv.appendChild(saveBtn);
+    contentDiv.appendChild(deleteBtn);
+    modalDiv.appendChild(contentDiv);
+    element.appendChild(modalDiv);
 
-var deleteBtn = document.createElement("button");
-deleteBtn.className = "save-img";
-var deleteImg = document.createElement("img");
-deleteImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/delete.png';
-deleteImg.style.height = '30px';
-deleteImg.style.width = '30px';
-deleteBtn.appendChild(deleteImg);
-deleteBtn.onclick = confirm;
+    var confDiv = document.createElement("div");
+    confDiv.className = "confirmation";
+    var confContDiv = document.createElement("div");
+    confContDiv.className = "confirmation-content";
+    var confTitle = document.createElement("h4");
+    var question = document.createTextNode("Are you sure?");
+    confTitle.appendChild(question);
 
+    var yesBtn = document.createElement("button");
+    var yesImg = document.createElement("img");
+    yesImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/ok.png'; 
+    yesImg.style.height = '30px';
+    yesImg.style.width = '30px';
+    yesBtn.style.marginRight = '20px';
+    yesBtn.appendChild(yesImg);
+    yesBtn.onclick = deleteElement;
 
-contentDiv.appendChild(title);
-contentDiv.appendChild(hr);
-contentDiv.appendChild(phonyDiv);
-contentDiv.appendChild(closeBtn);
-contentDiv.appendChild(saveBtn);
-contentDiv.appendChild(deleteBtn);
-modalDiv.appendChild(contentDiv);
-element.appendChild(modalDiv);
+    var noBtn = document.createElement("button");
+    var noImg = document.createElement("img");
+    noImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/close.png'; 
+    noImg.style.height = '30px';
+    noImg.style.width = '30px';
+    noBtn.appendChild(noImg);
+    noBtn.onclick = function() {
+        modalCnf.style.display = 'none';
+    };
 
-var confDiv = document.createElement("div");
-confDiv.className = "confirmation";
-var confContDiv = document.createElement("div");
-confContDiv.className = "confirmation-content";
-var confTitle = document.createElement("h4");
-var question = document.createTextNode("Are you sure?");
-confTitle.appendChild(question);
+    confContDiv.appendChild(confTitle);
+    confContDiv.appendChild(yesBtn);
+    confContDiv.appendChild(noBtn);
+    confDiv.appendChild(confContDiv);
+    element.appendChild(confDiv);
 
-var yesBtn = document.createElement("button");
-yesBtn.className = "save-img";
-var yesImg = document.createElement("img");
-yesImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/ok.png';
-yesImg.style.height = '30px';
-yesImg.style.width = '30px';
-yesBtn.appendChild(yesImg);
-yesBtn.onclick = deleteElement;
+    updateEditButtons();
+    let modalCnf = document.querySelector('.confirmation');
+    modal = document.querySelector('.modal');
+}
 
-var noBtn = document.createElement("button");
-noBtn.className = "save-img";
-var noImg = document.createElement("img");
-noImg.src = 'https://raw.githubusercontent.com/Tomoms/pssi/master/progetto/close.png';
-noImg.style.height = '30px';
-noImg.style.width = '30px';
-noBtn.appendChild(noImg);
-noBtn.onclick = function() {
-    modalCnf.style.display = 'none';
-};
-
-confContDiv.appendChild(confTitle);
-confContDiv.appendChild(yesBtn);
-confContDiv.appendChild(noBtn);
-confDiv.appendChild(confContDiv);
-element.appendChild(confDiv);
-
-let modalCnf = document.querySelector('.confirmation');
-setTimeout(updateEditButtons, 1000);
-var modal = document.querySelector('.modal');
+window.addEventListener('load', setTimeout(initEverything, 3000), false);
